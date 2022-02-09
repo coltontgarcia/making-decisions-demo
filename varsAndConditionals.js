7f7d7965b9f12e0ef7e05aa93f9ba4cfcd9cb78d
Colton Garcia <coltontgarcia@gmail.com>
2022-02-09 13:25:44 -0700

    finished demo

--- varsAndConditionals.js
+++ varsAndConditionals.js
@@ -1,5 +1,45 @@
-/*
-    In this file, we'll be using variables, conditionals,
-    and loops to play out a fight between Jon Snow and
-    Jamie Lannister.
-*/ 
+let eowynAttack = 50;
+let witchKingAttack = 45;
+
+if(eowynAttack > witchKingAttack) {
+    console.log("Eowyn is stronger.");
+} else if(witchKingAttack > eowynAttack) {
+    console.log("Witch King is stronger")
+} else {
+    console.log("They are the same strength.");
+};
+
+let eowynHealth = 100;
+let eowynDefense = 0;
+
+if(eowynHealth + eowynDefense <= witchKingAttack) {
+    console.log("Eowyn dies.");
+} else {
+    eowynHealth -= witchKingAttack
+    console.log(`She is down to ${eowynHealth}.`);
+};
+
+let coinLandsHeads = false;
+
+if(coinLandsHeads === true) {
+    console.log("Witch King gets to run away.");
+} else {
+    console.log ("Witch King got stabbed in the face");
+};
+
+//for(let i = 0; i < 3; i++) {
+//    if(eowynHealth > 0) {
+//        eowynHealth = eowynHealth - witchKingAttack;
+//        console.log(`Eowyn has ${eowynHealth}`);
+//     } else {
+//         console.log(`Eowyn dies.`);
+//     };
+//};
+
+while(eowynHealth > 0) {
+    eowynHealth -= witchKingAttack;
+    console.log(`Eowyn has ${eowynHealth} left.`);
+    if(eowynHealth <= 0) {
+        console.log("Eowyn has been slain.");
+    };
+};
